267372571532e7b83b7e813afd729005494c5407
Evgeny Safonov <jeka-luda@yandex.ru>
2024-04-15 13:18:12 +0300

    add ClientAdmin

--- mailings/admin.py
+++ mailings/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from mailings.models import Client
+
+
 # Register your models here.
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ("id", "fio", "email", "comment", "user",)
+    list_filter = ("user",)
+    search_fields = ("fio", "email",)
