bdbae865db1cc206eb667e17ad4290908e1d7974
Evgeny Safonov <jeka-luda@yandex.ru>
2024-04-15 11:29:49 +0300

    add urls and view

--- mailings/urls.py
+++ mailings/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 from mailings.apps import MailingsConfig
+from mailings.views import index
 
 app_name = MailingsConfig.name
 
 urlpatterns = [
-
+    path('', index)
 ]
