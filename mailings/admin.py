b153ac0d40d478d286420e405c917053d7f26ca3
Evgeny Safonov <jeka-luda@yandex.ru>
2024-04-16 19:37:42 +0300

    add model SettingsMailing

--- mailings/admin.py
+++ mailings/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from mailings.models import Client, Message
+from mailings.models import Client, Message, SettingsMailing
 
 
 # Register your models here.
@@ -16,3 +16,11 @@ class MessageAdmin(admin.ModelAdmin):
     list_display = ("id", "theme", "body", "user",)
     list_filter = ("user",)
     search_fields = ("theme", "body",)
+
+
+@admin.register(SettingsMailing)
+class SettingsMailingAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "clients", "message", "start_time", "end_time", "period", "status", "is_active",
+                    "user",)
+    list_filter = ("status", "period", "is_active", "user",)
+    search_fields = ("name", "clients",)
