91ff8fe392800fd20739babc47ca0bc87a1bc530
Evgeny Safonov <jeka-luda@yandex.ru>
2024-04-17 14:01:28 +0300

    add function runapscheduler

---
+++ mailings/management/commands/runapscheduler.py
@@ -0,0 +1,101 @@
+import logging
+
+from django.conf import settings
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+from django.core.management.base import BaseCommand
+from django_apscheduler.jobstores import DjangoJobStore
+from django_apscheduler.models import DjangoJobExecution
+from django_apscheduler import util
+
+from mailings.models import SettingsMailing
+
+logger = logging.getLogger(__name__)
+
+
+def day_mailing():
+    SettingsMailing.objects.filter(period="daily")
+    print("Every day")
+
+
+def weekly_mailing():
+    SettingsMailing.objects.filter(period="weekly")
+    print("Every week")
+
+
+def monthly_mailing():
+    SettingsMailing.objects.filter(period="monthly")
+    print("Every month")
+
+
+# The `close_old_connections` decorator ensures that database connections, that have become
+# unusable or are obsolete, are closed before and after your job has run. You should use it
+# to wrap any jobs that you schedule that access the Django database in any way.
+@util.close_old_connections
+def delete_old_job_executions(max_age=604_800):
+    """
+    This job deletes APScheduler job execution entries older than `max_age` from the database.
+    It helps to prevent the database from filling up with old historical records that are no
+    longer useful.
+
+    :param max_age: The maximum length of time to retain historical job execution records.
+                    Defaults to 7 days.
+    """
+    DjangoJobExecution.objects.delete_old_job_executions(max_age)
+
+
+class Command(BaseCommand):
+    help = "Runs APScheduler."
+
+    def handle(self, *args, **options):
+        scheduler = BlockingScheduler(timezone=settings.TIME_ZONE)
+        scheduler.add_jobstore(DjangoJobStore(), "default")
+
+        scheduler.add_job(
+            day_mailing,
+            trigger=CronTrigger(day="*"),  # Every day
+            id="day_mailing",  # The `id` assigned to each job MUST be unique
+            max_instances=1,
+            replace_existing=True,
+        )
+        logger.info("Added job 'day_mailing'.")
+
+        scheduler.add_job(
+            weekly_mailing,
+            trigger=CronTrigger(week="*"),  # Every week
+            id="weekly_mailing",  # The `id` assigned to each job MUST be unique
+            max_instances=1,
+            replace_existing=True,
+        )
+        logger.info("Added job 'weekly_mailing'.")
+
+        scheduler.add_job(
+            monthly_mailing,
+            trigger=CronTrigger(month="*"),  # Every month
+            id="monthly_mailing",  # The `id` assigned to each job MUST be unique
+            max_instances=1,
+            replace_existing=True,
+        )
+        logger.info("Added job 'monthly_mailing'.")
+
+        scheduler.add_job(
+            delete_old_job_executions,
+            trigger=CronTrigger(
+                day_of_week="mon", hour="00", minute="00"
+            ),  # Midnight on Monday, before start of the next work week.
+            id="delete_old_job_executions",
+            max_instances=1,
+            replace_existing=True,
+        )
+        logger.info(
+            "Added weekly job: 'delete_old_job_executions'."
+        )
+
+        try:
+            logger.info("Starting scheduler...")
+            scheduler.start()
+        except KeyboardInterrupt:
+            logger.info("Stopping scheduler...")
+            scheduler.shutdown()
+            logger.info("Scheduler shut down successfully!")
