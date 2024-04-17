4ead7029a690713d8aa73416629ee91559a164c7
Evgeny Safonov <jeka-luda@yandex.ru>
2024-04-17 18:00:40 +0300

    add function send_mailing

--- mailings/services.py
+++ mailings/services.py
@@ -1,27 +1,52 @@
-from smtplib import SMTPException
+import smtplib
 
-from django.core.mail import send_mail
 from django.conf import settings
-import django.utils.timezone
+from django.core.mail import send_mail
 from django.utils import timezone
 
-from mailings.models import Client, Message, SettingsMailing, LogsMailing
+from mailings.models import SettingsMailing, LogsMailing
 
 
 def day_mailing():
-    SettingsMailing.objects.filter(period="ежедневно")
-    print("Every day")
+    mailings = SettingsMailing.objects.filter(period="ежедневно").filter(status="запущена")
+    send_mailing(mailings)
 
 
 def weekly_mailing():
-    SettingsMailing.objects.filter(period="еженедельно")
-    print("Every week")
+    mailings = SettingsMailing.objects.filter(period="еженедельно").filter(status="запущена")
+    send_mailing(mailings)
 
 
 def monthly_mailing():
-    SettingsMailing.objects.filter(period="раз в месяц")
-    print("Every month")
+    mailings = SettingsMailing.objects.filter(period="раз в месяц").filter(status="запущена")
+    send_mailing(mailings)
 
 
-def send_mailing():
-    pass
+def send_mailing(mailings):
+    now = timezone.now()
+    for mailing in mailings:
+        clients = [client.email for client in mailing.clients.all()]
+        try:
+            server_response = send_mail(subject=mailing.message_set.theme,
+                                        message=mailing.message_set.body,
+                                        from_email=settings.EMAIL_HOST_USER,
+                                        recipient_list=clients,
+                                        fail_silently=False)
+            log = LogsMailing.objects.create(date_time_last_attempt=now,
+                                       answer_server=server_response,
+                                       is_status=True,
+                                       mailing=mailing)
+            log.save()
+        except smtplib.SMTPException as e:
+            log = LogsMailing.objects.create(date_time_last_attempt=now,
+                                       answer_server=e,
+                                       is_status=False,
+                                       mailing=mailing)
+            log.save()
+        except Exception as e:
+            log = LogsMailing.objects.create(date_time_last_attempt=now,
+                                       answer_server=e,
+                                       is_status=False,
+                                       mailing=mailing)
+            log.save()
+        return log
